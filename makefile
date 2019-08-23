a0b47dca6a45668ce329bd1e5e9143445b9a47a8
rust401 <zhaodonghu94@gmail.com>
2019-08-23 07:08:53 -0700

    updata

--- makefile
+++ makefile
@@ -1,25 +1,27 @@
 #all: test.cpp i s att o ll
-all: test.cpp
-	$(COMPILER) -std=c++11 -g -o test test.cpp $(LEVEL)
-	time ./test
 
-LEVEL=-O0
+TARGET=scheduler.cpp
+LEVEL=-O3
 COMPILER= clang++
 
-i:test.cpp
-	 $(COMPILER) -E test.cpp -o test.i $(LEVEL)
+all: $(TARGET)
+	$(COMPILER) -std=c++11 -g -o test $(TARGET) $(LEVEL)
+	time ./test
 
-ll:test.cpp
-	 $(COMPILER) -S -emit-llvm test.cpp $(LEVEL)
+i:$(TARGET)
+	 $(COMPILER) -E $(TARGET) -o test.i $(LEVEL)
 
-s:test.cpp
-	 $(COMPILER) -S -mllvm --x86-asm-syntax=intel test.cpp $(LEVEL)
+ll:$(TARGET)
+	 $(COMPILER) -S -emit-llvm $(TARGET) $(LEVEL)
 
-att:test.cpp
-	 $(COMPILER) -S test.cpp -o test_att.s $(LEVEL)
+s:$(TARGET)
+	 $(COMPILER) -S -mllvm --x86-asm-syntax=intel $(TARGET) $(LEVEL)
 
-o:test.cpp
-	 $(COMPILER) -c test.cpp -o test.o $(LEVEL)
+att:$(TARGET)
+	 $(COMPILER) -S $(TARGET) -o test_att.s $(LEVEL)
+
+o:$(TARGET)
+	 $(COMPILER) -c $(TARGET) -o test.o $(LEVEL)
 
 clean:
-	rm -f peda* .gdb*
+	rm -f peda* .gdb* *.i *.ll *.s *.o
--- test.cpp
+++ test.cpp
@@ -1,29 +1,61 @@
-#include <bits/stdc++.h>
+#include<stdio.h>
+#include<memory>
+#include<vector>
+#include<iostream>
+#include<unistd.h>
+#include<random>
+#include<time.h>
 
-
+const std::string fileName="./hanpi.txt";
 FILE* fp;
+
+static std::default_random_engine e(time(0));
+static std::uniform_real_distribution<float> u(0,2);
+
+std::vector<double> getDudeBeforeAvg(const double dude){
+    double div_1=u(e);
+    double div_2=-u(e);
+    double div_3=0-div_1-div_2;
+    return {dude+div_1,dude+div_2,dude+div_3};
+}
+
+std::vector<std::vector<double> > getResult(const std::vector<double>& inputValue){
+    std::vector<std::vector<double>> result;
+    for(auto dude:inputValue){
+        result.push_back(getDudeBeforeAvg(dude));
+    }
+    return result;
+}
+
+void Display(const std::vector<std::vector<double>>& result){
+    for(auto dude:result){
+        for(int i=0;i<dude.size();++i){
+            std::cout<<dude[i]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+}
+
+std::vector<double> readFromFile(FILE* fp){
+    double input,output;
+    std::vector<double> result;
+    if((fp=fopen(fileName.c_str(),"r"))==nullptr){
+        printf("File open error!\n");
+        exit(0);
+    }
+    fseek(fp,0L,SEEK_END);
+    uint64_t size=ftell(fp);
+    rewind(fp);
+    while(ftell(fp)<size){
+        fscanf(fp,"%lf\t%lf\n",&input,&output);
+        result.push_back(input);
+        result.push_back(output);
+    }
+    return result;
+}
 
 int main()
 {
-    double a,b;
-    fp=fopen("./hanpi.txt","r");
-    if(fp==nullptr)exit(0);
-    fseek(fp,0L,SEEK_END);
-    uint64_t size=ftell(fp);
-    std::cout<<size<<std::endl;
-    rewind(fp);
-
-    while(ftell(fp)<size){
-        std::cout<<ftell(fp)<<std::endl;
-        fscanf(fp,"%lf\t%lf\n",&a,&b);
-        std::cout<<fp<<std::endl;
-        std::cout<<ftell(fp)<<std::endl;
-        std::cout<<a<<" "<<b<<std::endl;
-        std::cout<<" "<<std::endl;
-        
-        
-    }
-
-    fclose(fp);
+    Display(getResult(readFromFile(fp)));
     return 0;
 }
